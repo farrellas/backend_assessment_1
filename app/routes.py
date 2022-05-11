384aebeabe6999e06eca1a0764c772d1df508a02
Andrew Farrell <farrellas05@yahoo.com>
2022-05-10 20:17:40 -0600

    completed primary work

--- app/routes.py
+++ app/routes.py
@@ -2,44 +2,63 @@ from app import app
 from flask import request
 import json
 
+
 with open('app/data.json') as file:
     data = json.load(file)
 
 
+def recipe_exists(recipe_name):
+    for recipe in data["recipes"]:
+        if recipe["name"] == recipe_name:
+            return True
+    return False
+
+
 @app.route('/recipes')
 def get_recipes():
-    recipeNames = [recipe["name"] for recipe in data["recipes"]]
-    return {
-        'recipeNames': recipeNames
-    }
+    recipe_names = [recipe["name"] for recipe in data["recipes"]]
+    return {'recipes': recipe_names}, 200
 
 
-@app.route('/recipes/details/<string:recipeName>')
-def get_recipe_details(recipeName):
-    this_recipe = {}
+@app.route('/recipes/details/<string:recipe_name>')
+def get_recipe_details(recipe_name):
     for recipe in data["recipes"]:
-        if recipe["name"] == recipeName:
-            this_recipe = recipe
-    try:
-        return {
-            "details": {
-                "ingredients": this_recipe["ingredients"],
-                "numSteps": len(this_recipe["instructions"])
-            }
-        }
-    except:
-        return {}
+        if recipe["name"] == recipe_name:
+            return {
+                "details": {
+                    "ingredients": recipe["ingredients"],
+                    "numSteps": len(recipe["instructions"])
+                }
+            }, 200
+    return {}, 200
+
 
 @app.route('/recipes', methods=["POST"])
 def add_new_recipe():
-    exists = False
-    new_recipe = request.get_json()
-    for recipe in data["recipes"]:
-        if recipe["name"] == new_recipe["name"]:
-            exists = True
+    input_recipe = request.get_json()
+
+    if recipe_exists(input_recipe["name"]):
+        return {'error': "Recipe already exists"}, 400
+    else:
+        data["recipes"].append(input_recipe)
+        output_data = json.dumps(data, indent=4)
+        with open('app/data.json', 'w') as outfile:
+            outfile.write(output_data)
+        return '', 201
+
+
+@app.route('/recipes', methods=["PUT"])
+def update_recipe():
+    input_recipe = request.get_json()
+
+    exists = recipe_exists(input_recipe["name"])
+
     if exists:
-        return {'error': "Recipe already exists"}
-    if not exists:
-        print(new_recipe)
-        data["recipes"].append(new_recipe)
-    return '' 
+        for i in range(len(data["recipes"])):
+            if data["recipes"][i]["name"] == input_recipe["name"]:
+                data["recipes"][i] = input_recipe
+        output_data = json.dumps(data, indent=4)
+        with open('app/data.json', 'w') as outfile:
+            outfile.write(output_data)
+        return '', 204
+    return {'error': "Recipe does not exist"}, 404
